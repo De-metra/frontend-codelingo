8c77b3afb21118353ee98a6138f5824a89c907da
De-metra <dariadubrovskay@gmail.com>
2026-05-07 16:35:58 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'achievments/index.html'),
+        auth: resolve(__dirname, 'auth/index.html'),
+        auth: resolve(__dirname, 'courses/index.html'),
+        auth: resolve(__dirname, 'lesson/index.html'),
+        auth: resolve(__dirname, 'profile/index.html'),
+        auth: resolve(__dirname, 'profile-settings/index.html'),
+        auth: resolve(__dirname, 'track/index.html'),
+      },
+    },
+  },
 })
